7f2276624cfeb83af2a569a89fae4186fb28ac98
LuesewrBDO <laurens.de.swart@bdo.nl>
2026-06-23 13:33:13 +0200

    Make text body property of slide an XmlElementProperty

--- src/pptx_editor/xml_elements/shape.py
+++ src/pptx_editor/xml_elements/shape.py
@@ -1,25 +1,13 @@
-from typing import TYPE_CHECKING, TypeGuard
-
-from pptx_editor.xml_element import XmlElement
+from pptx_editor.xml_element import XmlElement, XmlElementProperty
 from pptx_editor.xml_elements.text import TextBody
-from pptx_editor.exceptions import PowerpointIntegrityError
 
-if TYPE_CHECKING:
-    from pptx_editor.parts.slide import Slide
 
 class Shape(XmlElement):
     default_namespace = 'http://schemas.openxmlformats.org/presentationml/2006/main'
     default_prefix = 'p'
     default_name = 'sp'
 
-    @property
-    def text_body(self) -> 'TextBody | None':
-        text_body = self.get_element('txBody', 'p')
-
-        if text_body is not None and not isinstance(text_body, TextBody):
-            raise PowerpointIntegrityError('The txBody element in the shape element is not of the expected type.')
-
-        return text_body
+    text_body: TextBody | None = XmlElementProperty(TextBody)
 
 class NonVisualShapeProperties(XmlElement):
     default_namespace = 'http://schemas.openxmlformats.org/presentationml/2006/main'
