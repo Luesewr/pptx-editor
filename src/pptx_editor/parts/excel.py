5609d13c04a5c163fa2c446f14e6550c300733eb
LuesewrBDO <laurens.de.swart@bdo.nl>
2026-07-24 23:07:29 +0200

    Remove forgotten debug print

--- src/pptx_editor/parts/excel.py
+++ src/pptx_editor/parts/excel.py
@@ -6,7 +6,3 @@ class ExcelWorksheet(Part):
     default_base_path = PurePosixPath('/ppt/embeddings')
     default_part_name = 'Microsoft_Excel_Worksheet'
     default_extension = 'xlsx'
-
-    def __init__(self, main_part: 'Part', file_path: PurePosixPath | None = None, content_type: str | None = None, is_default: bool = False):
-        super().__init__(main_part, file_path, content_type, is_default)
-        print("hey")
